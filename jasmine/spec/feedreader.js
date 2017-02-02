1594b6286cc646160511f445be92e472b1530eb4
madison kanna <madisonkanna@gmail.com>
2017-02-02 13:56:56 -0800

    Ran through JS beautifier

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,19 +1,17 @@
-
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -44,7 +42,7 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-    
+
         it('all feed names are defined', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeDefined();
@@ -70,11 +68,11 @@ $(function() {
             expect(hiddenMenu).toBe(true);
         });
 
-         /* Wrote a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        /* Wrote a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
 
         it('The menu visibility changed on click', function() {
             hamburger.click();
@@ -120,13 +118,13 @@ $(function() {
         var newEntry;
 
         beforeEach(function(done) {
-        //We must have two different feeds;
+            //We must have two different feeds;
             loadFeed(1, function() {
                 originalEntry = $('.feed').text();
                 loadFeed(0, function() {
                     newEntry = $('.feed').text();
                     done();
-                    
+
                 });
             });
         });
